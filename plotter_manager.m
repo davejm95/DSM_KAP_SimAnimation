fc1bf0568aaea36d9ad8d00943e248b4c514b141
David Metzger <dave.m@solnet.ch>
2019-03-31 05:30:37 +0100

    add image to readme

--- plotter_manager.m
+++ plotter_manager.m
@@ -4,16 +4,17 @@ clearvars
 %how many times to repeat
 repeatAnimation=5;
 %option to play animation frame by frame by pressing enter
-frameByFrame=false;
+frameByFrame=true;
 %animation speed, default = 1= realtime
 simSpeed=1; %or manually set tstep after reading input data
 
 %specify simulation input data
-Filename='sample_data/rower4_kap_v2.kap';
-%Filename='4BAR.KAP';
+%Filename='sample_data/rower4_kap_v2.kap';
+Filename='sample_data/4BAR.KAP';
 
 %load the simulation results
-load('sample_data/plt_rower4.mat');
+%load('sample_data/plt_rower4.mat');
+load('sample_data/plt4bar.mat');
 
 ReadInputData
 plotter_bodypoints
